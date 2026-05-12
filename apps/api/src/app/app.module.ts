56dc0decd5a3b689c3aee158159ae59ece849559
Võ Lê Duy Anh <anhvo.leduy@outlook.com>
2026-05-12 15:15:37 +0700

    fix(api): register AppController + AppService in AppModule

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 import { BoardsModule } from '../boards/boards.module';
@@ -44,5 +46,7 @@ import { CashFlowModule } from '../cash-flow/cash-flow.module';
     EntriesModule,
     CashFlowModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
